ef02652ce292bf1ef2784b2d93ce2618b51e1213
Hunter Wodzenski <h.wodzenski@gmail.com>
2017-02-20 18:35:03 -0500

    Update developer.php

--- developer.php
+++ developer.php
@@ -234,7 +234,7 @@ if(isset($_SESSION['id'])) {
                     <li><a style="font-family: 'Righteous', cursive;" href="playercard.php">Playercard</a></li>
                     <li style="background: none;"><a title="Under Development" style="color: darkred;font-family: 'Righteous', cursive;" href="#0">Game&nbsp;Info</a></li>
                     <li style="background: none;"><a title="Under Development" style="color: darkred;font-family: 'Righteous', cursive;" href="#0">PB&nbsp;Analytics</a></li>
-                    <li><a style="font-family: 'Righteous', cursive;" href="#"  style="color: gray; text-decoration: overline;>Developer</a></li>
+                    <li><a style="font-family: 'Righteous', cursive;" href="#"  style="color: gray; text-decoration: overline;">Developer</a></li>
                     <li><a style="font-family: 'Righteous', cursive;" href="includes/logout.inc.php">Logout</a></li>
                 </ul>
               </div>
