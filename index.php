4f7b9e5966f3a146bfbbb55b82adbbb47501e11b
Hunter Wodzenski <h.wodzenski@gmail.com>
2017-02-22 09:09:49 -0500

    gadsense

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
 <html>
     <!--Copyright 2017 PvPCrunch-->
     <head>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-2104547321871579",
+            enable_page_level_ads: true
+          });
+        </script>
         <title>
             PVPCrunch.COM
         </title>
